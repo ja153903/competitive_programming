f6902b4bb68cb26154c3c45142e26dfcad51303b
ja153903 <jaimeabbariao@gmail.com>
2023-01-11 22:15:00 -0500

    feat: updating with daily

---
+++ leetcode/monthly/jan2023/day12.py
@@ -0,0 +1,36 @@
+from collections import defaultdict, Counter
+
+
+class Solution:
+    def countSubTrees(self, n: int, edges: list[list[int]], labels: str) -> list[int]:
+        def dfs(node: int) -> Counter:
+            cnt = Counter()
+
+            if node not in visited:
+                cnt[labels[node]] += 1
+
+                visited.add(node)
+
+                # the idea with this solution is that at every level of the tree
+                # we have built up a series of Counter objects that we can update
+                # to get the updated count from the subtrees
+                # this means that the leaves of the tree will have the smallest possible counter
+                # and the counter bubbles up and becomes larger.
+                for child in graph.get(node, []):
+                    cnt += dfs(child)
+
+                res[node] = cnt[labels[node]]
+
+            return cnt
+
+        res = [0] * n
+        graph = defaultdict(list)
+        visited = set()
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        dfs(0)
+
+        return res
